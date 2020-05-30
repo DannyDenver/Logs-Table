61e2c45d072e8dafa5278f75ac6eea74bdafcfea
DannyDenver <djt09d@fulbrightmail.org>
2020-05-30 14:34:48 -0600

    make table mobile friendly

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,8 +1,8 @@
 import { TableColumn } from './models/table-column.model';
 
 export const errorTable = [
-    new TableColumn('ID', 'id', true),
-    new TableColumn('Error', 'error', true),
-    new TableColumn('Created At', 'created_at', true),
-    new TableColumn('Backtrace', 'backtrace', false, false)
+    new TableColumn('ID', 'id', '15vw', true),
+    new TableColumn('Error', 'error', '60vw', true),
+    new TableColumn('Created At', 'created_at', '15vw', true),
+    new TableColumn('Backtrace', 'backtrace', null, false, false)
 ]
--- src/app/models/table-column.model.ts
+++ src/app/models/table-column.model.ts
@@ -1,12 +1,15 @@
 export class TableColumn {
-    constructor(label: string, key: string, sortable?: boolean, isDisplayed = true) {
+    constructor(label: string, key: string, columnWidth: string, sortable?: boolean, isDisplayed = true) {
         this.label = label,
         this.key = key,
+        this.columnWidth = columnWidth,
         this.sortable = sortable,
         this.isDisplayed = isDisplayed
     }
+    
     label: string;
     key: string;
+    columnWidth: string;
     sortable = false;
     isDisplayed = true;
 }
